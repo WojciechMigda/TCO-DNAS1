bde0644e1b171b329540ffbbc57daeb18636c9cf
Wojciech Migda <wojtek.golf@interia.pl>
2016-04-17 21:27:33 +0200

    tokenizing with offsets

--- src/dna_sequencing.hpp
+++ src/dna_sequencing.hpp
@@ -44,6 +44,7 @@ struct DNASequencing
     {
         m_curr_chromaid = -1;
         m_chroma_off = 0;
+        m_sub_chroma_off = 0;
         m_subchroma.clear();
         m_chromatid_bw_contexts.clear();
     }
@@ -75,10 +76,6 @@ struct DNASequencing
         // process subchroma collected so far
         if (m_subchroma.size() >= MIN_SUBCHROMA_SZ)
         {
-//            std::stringstream ss(m_subchroma);
-//            std::vector<gx::DnaNucleobase> subchroma;
-//
-//            ss >> subchroma;
             // PY: first_occurence = FirstOccurence(Text)
             // FirstOccurence(text) -> indeksy pierwszych wystapien w posortowanym text
             // text = ACATGCTACTTT$
@@ -141,28 +138,32 @@ suffarr           7        5     ? 11       10     (index w text)
             m_subchroma.push_back('$');
             const auto & subchroma = m_subchroma;
 
-//            const auto full_suffix_array = BW::full_suffix_array(std::string("ACATGCTACTTT$"));
-//            const auto last_col = BW::last_column(std::string("ACATGCTACTTT$"), full_suffix_array);
-//            const auto count = BW::count(last_col, 1);
-//            const auto count3 = BW::count(last_col, 3);
+//            {
+//                const auto full_suffix_array = BW::full_suffix_array(std::string("ACATGCTACTTT$"));
+//                const auto last_col = BW::last_column(std::string("ACATGCTACTTT$"), full_suffix_array);
+//                const auto count = BW::count(last_col, 1);
+//                const auto count3 = BW::count(last_col, 3);
 //
-//            std::vector<BW::count_type> foo(last_col.size() + 1);
-//            for (std::size_t ix{0}; ix < foo.size(); ++ix)
-//            {
-//                foo[ix] = count3.value(4, ix, last_col);
+//                std::vector<BW::count_type> foo(last_col.size() + 1);
+//                for (std::size_t ix{0}; ix < foo.size(); ++ix)
+//                {
+//                    foo[ix] = count3.value(4, ix, last_col);
+//                }
+//                const auto first_occ = BW::first_occurences(count3, last_col);
+//                const auto a = BW::ix_by_base('$');
+//                const auto b = BW::ix_by_base('A');
+//                const auto c = BW::ix_by_base('C');
+//                const auto d = BW::ix_by_base('G');
+//                const auto e = BW::ix_by_base('T');
+//                const auto partial_sufarr = BW::partial_suffix_array(full_suffix_array, 3);
+//                std::vector<std::size_t> bar(full_suffix_array.size());
+//                for (std::size_t ix{0}; ix < bar.size(); ++ix)
+//                {
+//                    bar[ix] = partial_sufarr.value(ix, last_col, count3, first_occ);
+//                }
+//                const auto res = BW::better_match("CATG", BW::Context{last_col, count3, first_occ, partial_sufarr});
 //            }
-//            const auto first_occ = BW::first_occurences(count3, last_col);
-//            const auto a = BW::ix_by_base('$');
-//            const auto b = BW::ix_by_base('A');
-//            const auto c = BW::ix_by_base('C');
-//            const auto d = BW::ix_by_base('G');
-//            const auto e = BW::ix_by_base('T');
-//            const auto partial_sufarr = BW::partial_suffix_array(full_suffix_array, 3);
-//            std::vector<std::size_t> bar(full_suffix_array.size());
-//            for (std::size_t ix{0}; ix < bar.size(); ++ix)
-//            {
-//                bar[ix] = partial_sufarr.value(ix, last_col, count3, first_occ);
-//            }
+
             auto full_suffix_array = BW::full_suffix_array(subchroma);
             const auto last_col = BW::last_column(subchroma, full_suffix_array);
             const auto count = BW::count(last_col, 256);
@@ -170,16 +171,18 @@ suffarr           7        5     ? 11       10     (index w text)
             const auto partial_sufarr = BW::partial_suffix_array(full_suffix_array, 256);
             full_suffix_array.clear();
 
-            m_chromatid_bw_contexts[m_curr_chromaid].push_back(BW::Context{last_col, count, first_occ, partial_sufarr});
+            m_chromatid_bw_contexts[m_curr_chromaid].emplace_back(
+                m_sub_chroma_off, BW::Context{last_col, count, first_occ, partial_sufarr});
 
 //            std::cerr << "[DNAS1] " << m_subchroma.size() << std::endl;
         }
-        if (m_subchroma.size())
+        if (m_subchroma.size()) // temporary for reference logging
         {
-            std::cerr << "[DNAS1] " << m_subchroma.size() << std::endl;
+            std::cerr << "[DNAS1] " << m_subchroma.size() << ", off: " << m_sub_chroma_off << std::endl;
         }
 
         m_subchroma.clear();
+        m_sub_chroma_off = m_chroma_off;
     }
 
     int passReferenceGenome(
@@ -190,34 +193,26 @@ suffarr           7        5     ? 11       10     (index w text)
         {
             // reset offset with the chromatid when we start receiving a new one
             m_chroma_off = 0;
+            m_sub_chroma_off = 0;
         }
         m_curr_chromaid = chromatidSequenceId;
 
         for (const auto & chroma_piece : chromatidSequence)
         {
             // tokenizer
-//            typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
-//            boost::char_separator<char> sep("N", "", boost::drop_empty_tokens);
-//            tokenizer tokens(chroma_piece, sep);
-//            for (tokenizer::iterator tok_iter = tokens.begin();
-//                     tok_iter != tokens.end();
-//                     ++tok_iter)
-//            {
-//                const auto off = tok_iter.base() - chroma_piece.begin() - tok_iter->size();
-//
-//                //
-//            }
+            typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+            boost::char_separator<char> sep("N", "", boost::drop_empty_tokens);
+            tokenizer tokens(chroma_piece, sep);
 
+            std::vector<std::pair<uint32_t, std::string>> Npieces;
 
-            std::vector<std::string> Npieces;
-            boost::split(Npieces, chroma_piece, [](char c){return c == 'N';}, boost::token_compress_on);
-            if (Npieces.back().empty())
+            for (tokenizer::iterator tok_iter = tokens.begin();
+                     tok_iter != tokens.end();
+                     ++tok_iter)
             {
-                Npieces.pop_back();
-            }
-            if (Npieces.front().empty())
-            {
-                Npieces.erase(Npieces.begin());
+                const uint32_t offset = tok_iter.base() - chroma_piece.begin() - tok_iter->size();
+
+                Npieces.emplace_back(offset, *tok_iter);
             }
 
 
@@ -231,20 +226,22 @@ suffarr           7        5     ? 11       10     (index w text)
                 // process all excluding the last one
                 for (std::size_t ix{0}; ix < Npieces.size() - 1; ++ix)
                 {
-                    m_subchroma.append(Npieces[ix]);
+                    m_sub_chroma_off += Npieces[ix].first;
+                    m_subchroma.append(Npieces[ix].second);
                     close_sub_chroma();
                 }
 
                 // do the last one, but without closing
-                m_subchroma.append(Npieces.back());
+                m_sub_chroma_off += Npieces.back().first;
+                m_subchroma.append(Npieces.back().second);
             }
+
+            m_chroma_off += chroma_piece.size();
 
             if (chroma_piece.back() == 'N')
             {
                 close_sub_chroma();
             }
-
-            m_chroma_off += chroma_piece.size();
         }
 
         return 0;
@@ -257,30 +254,32 @@ suffarr           7        5     ? 11       10     (index w text)
         const std::vector<std::string> & readName,
         const std::vector<std::string> & readSequence)
     {
-//        for (std::size_t ix{0}; ix < readName.size(); ix += 2)
-//        {
-//            const auto & head_name = readName[ix];
-//            const auto & tail_name = readName[ix + 1];
-//            const auto & head_read = readSequence[ix];
-//            const auto & tail_read = readSequence[ix + 1];
-//
-//            for (const auto & chroma_bw : m_chromatid_bw_contexts)
-//            {
-//                const auto chroma_id = chroma_bw.first;
-//
-//                for (const auto & bw_ctx : chroma_bw.second)
-//                {
-//                    const auto matched = BW::better_match(head_read, bw_ctx);
-//                    if (matched.size())
-//                    {
-//                        std::cerr << "[DNAS1] matched " << matched.size() << std::endl;
-//                        std::cerr << "[DNAS1] " << matched.front() << std::endl;
-//                        std::cerr << "[DNAS1] " << head_read << std::endl;
-////                        std::strncmp(head_read.c_str(), );
-//                    }
-//                }
-//            }
-//        }
+        for (std::size_t ix{0}; ix < readName.size(); ix += 2)
+        {
+            const auto & head_name = readName[ix];
+            const auto & tail_name = readName[ix + 1];
+            const auto & head_read = readSequence[ix];
+            const auto & tail_read = readSequence[ix + 1];
+
+            for (const auto & chroma_bw : m_chromatid_bw_contexts)
+            {
+                const auto chroma_id = chroma_bw.first;
+
+                for (const auto & offset_bw_ctx : chroma_bw.second)
+                {
+                    const auto offset = offset_bw_ctx.first;
+                    const auto & bw_ctx = offset_bw_ctx.second;
+                    const auto matched = BW::better_match(head_read, bw_ctx);
+                    if (matched.size())
+                    {
+                        std::cerr << "[DNAS1] matched " << matched.size() << std::endl;
+                        std::cerr << "[DNAS1] " << matched.front() + offset << std::endl;
+                        std::cerr << "[DNAS1] " << head_read << std::endl;
+//                        std::strncmp(head_read.c_str(), );
+                    }
+                }
+            }
+        }
 
 
         std::vector<std::string> ret(N, "");
@@ -295,10 +294,23 @@ suffarr           7        5     ? 11       10     (index w text)
         return ret;
     }
 
+    // awful state, ugh:
+
+    // chromatid id, passed with each passReferenceGenome
     int m_curr_chromaid;
+    // global position within chromatid being collected (across calls to passReferenceGenome)
+    // irrelevant of splits
+    // shall be reset for each new chromatid
     std::size_t m_chroma_off;
+    // global position within chromatid being collected (across calls to passReferenceGenome)
+    // irrelevant of splits
+    // which points to the start of subchromatid being collected
+    std::size_t m_sub_chroma_off;
+    // subsection of chromatid being collected,
+    // subsections are separated by Ns in a chromatid
     std::string m_subchroma;
-    std::unordered_map<int, std::vector<BW::Context>> m_chromatid_bw_contexts;
+    // lists of BW contexts for each chromatid collected
+    std::unordered_map<int, std::vector<std::pair<uint32_t, BW::Context>>> m_chromatid_bw_contexts;
 };
 
 #endif /* DNA_SEQUENCING_HPP_ */
