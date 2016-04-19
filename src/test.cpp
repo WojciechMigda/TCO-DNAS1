200b576b30e5fa995e77b5078037a5eeef818d31
Wojciech Migda <wojtek.golf@interia.pl>
2016-04-19 20:40:58 +0200

    BW unit tests

--- src/test.cpp
+++ src/test.cpp
@@ -33,11 +33,48 @@
 
 using ::testing::ElementsAre;
 using ::testing::ElementsAreArray;
+using ::testing::StrEq;
 
-TEST(SuffixArray, IsConstructedFromPattern)
+
+TEST(FullSuffixArray, IsConstructedFromPattern_303_4)
+{
+    const std::string pattern("AATCGGGTTCAATCGGGGT$");
+    const std::vector<BW::pos_type> res = BW::full_suffix_array(pattern);
+    ASSERT_THAT(res, ElementsAreArray({19, 10, 0, 11, 1, 9, 13, 3, 14, 15, 4, 16, 5, 17, 6, 18, 8, 12, 2, 7}));
+}
+
+
+TEST(FullSuffixArray, IsConstructedFromPattern_304_6)
 {
     const std::vector<BW::pos_type> res = BW::full_suffix_array(std::string("ACATGCTACTTT$"));
     ASSERT_THAT(res, ElementsAreArray({12, 0, 7, 2, 1, 5, 8, 4, 11, 6, 3, 10, 9}));
+}
+
+
+TEST(FullSuffixArray, IsConstructedFromPattern_310_2)
+{
+    const std::vector<BW::pos_type> res = BW::full_suffix_array(std::string("AACGATAGCGGTAGA$"));
+    ASSERT_THAT(res, ElementsAreArray({15, 14, 0, 1, 12, 6, 4, 2, 8, 13, 3, 7, 9, 10, 11, 5}));
+}
+
+
+TEST(LastColumn, IsConstructedFromPattern303_4)
+{
+    const std::string pattern("AATCGGGTTCAATCGGGGT$");
+    const std::vector<BW::pos_type> full_suffix_array = BW::full_suffix_array(pattern);
+    const std::string res = BW::last_column(pattern, full_suffix_array);
+
+    ASSERT_THAT(res, StrEq("TC$AATTTCGCGGGGGTAAG"));
+}
+
+
+TEST(LastColumn, IsConstructedFromPattern304_6)
+{
+    const std::string pattern("ACATGCTACTTT$");
+    const std::vector<BW::pos_type> full_suffix_array = BW::full_suffix_array(pattern);
+    const std::string res = BW::last_column(pattern, full_suffix_array);
+
+    ASSERT_THAT(res, StrEq("T$TCAGATTCATC"));
 }
 
 
