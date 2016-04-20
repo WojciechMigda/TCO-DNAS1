2d414a179ead6a6ac205d46867c9bc5033e31768
Wojciech Migda <wojtek.golf@interia.pl>
2016-04-20 23:38:37 +0200

    boost is no more

--- src/amalgamate.cpp
+++ src/amalgamate.cpp
@@ -1,16 +1,3 @@
-//#pragma GCC optimize ( "-Os" )
-
-//#define BOOST_NO_USER_CONFIG 1
-
-//#include "boost/config/compiler/gcc.hpp"
-//#define BOOST_NO_COMPILER_CONFIG
-
-//#include "boost/config/stdlib/libstdcpp3.hpp"
-//#define BOOST_NO_STDLIB_CONFIG
-
-//#include "boost/config/platform/linux.hpp"
-//#define BOOST_NO_PLATFORM_CONFIG
-
 #define NDEBUG
 
 #include "dna_sequencing.hpp"
